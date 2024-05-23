8bb2f8ecc3a8696c932b85945f2aad230df25c47
thangchung <thangchung@ymail.com>
2024-05-23 17:07:52 +0700

    upgrade to .NET Aspire GA

--- service-defaults/Extensions.cs
+++ service-defaults/Extensions.cs
@@ -12,91 +12,91 @@ namespace Microsoft.Extensions.Hosting;
 
 public static class Extensions
 {
-    public static IHostApplicationBuilder AddServiceDefaults(this IHostApplicationBuilder builder)
-    {
-        builder.ConfigureOpenTelemetry();
+	public static IHostApplicationBuilder AddServiceDefaults(this IHostApplicationBuilder builder)
+	{
+		builder.ConfigureOpenTelemetry();
 
-        builder.AddDefaultHealthChecks();
+		builder.AddDefaultHealthChecks();
 
-        builder.Services.AddServiceDiscovery();
+		builder.Services.AddServiceDiscovery();
 
-        builder.Services.ConfigureHttpClientDefaults(http =>
-        {
-            // Turn on resilience by default
-            http.AddStandardResilienceHandler();
+		builder.Services.ConfigureHttpClientDefaults(http =>
+		{
+			// Turn on resilience by default
+			http.AddStandardResilienceHandler();
 
-            // Turn on service discovery by default
-            http.AddServiceDiscovery();
-        });
+			// Turn on service discovery by default
+			http.AddServiceDiscovery();
+		});
 
-        return builder;
-    }
+		return builder;
+	}
 
-    public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
-    {
-        builder.Logging.AddOpenTelemetry(logging =>
-        {
-            logging.IncludeFormattedMessage = true;
-            logging.IncludeScopes = true;
-        });
+	public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
+	{
+		builder.Logging.AddOpenTelemetry(logging =>
+		{
+			logging.IncludeFormattedMessage = true;
+			logging.IncludeScopes = true;
+		});
 
-        builder.Services.AddOpenTelemetry()
-            .WithMetrics(metrics =>
-            {
-                metrics.AddAspNetCoreInstrumentation()
-                    .AddHttpClientInstrumentation()
-                    .AddRuntimeInstrumentation();
-            })
-            .WithTracing(tracing =>
-            {
-                tracing.AddAspNetCoreInstrumentation()
-                       .AddHttpClientInstrumentation()
+		builder.Services.AddOpenTelemetry()
+			.WithMetrics(metrics =>
+			{
+				metrics.AddAspNetCoreInstrumentation()
+					.AddHttpClientInstrumentation()
+					.AddRuntimeInstrumentation();
+			})
+			.WithTracing(tracing =>
+			{
+				tracing.AddAspNetCoreInstrumentation()
+					   .AddHttpClientInstrumentation()
 					   .AddSource("MassTransit")
 					   .AddSource("Marten");
-            });
+			});
 
-        builder.AddOpenTelemetryExporters();
+		builder.AddOpenTelemetryExporters();
 
-        return builder;
-    }
+		return builder;
+	}
 
-    private static IHostApplicationBuilder AddOpenTelemetryExporters(this IHostApplicationBuilder builder)
-    {
-        var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+	private static IHostApplicationBuilder AddOpenTelemetryExporters(this IHostApplicationBuilder builder)
+	{
+		var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
 
-        if (useOtlpExporter)
-        {
-            builder.Services.AddOpenTelemetry().UseOtlpExporter();
-        }
+		if (useOtlpExporter)
+		{
+			builder.Services.AddOpenTelemetry().UseOtlpExporter();
+		}
 
-        return builder;
-    }
+		return builder;
+	}
 
-    public static IHostApplicationBuilder AddDefaultHealthChecks(this IHostApplicationBuilder builder)
-    {
-        builder.Services.AddHealthChecks()
-            // Add a default liveness check to ensure app is responsive
-            .AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
+	public static IHostApplicationBuilder AddDefaultHealthChecks(this IHostApplicationBuilder builder)
+	{
+		builder.Services.AddHealthChecks()
+			// Add a default liveness check to ensure app is responsive
+			.AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
 
-        return builder;
-    }
+		return builder;
+	}
 
-    public static WebApplication MapDefaultEndpoints(this WebApplication app)
-    {
-        // Adding health checks endpoints to applications in non-development environments has security implications.
-        // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
-        if (app.Environment.IsDevelopment())
-        {
-            // All health checks must pass for app to be considered ready to accept traffic after starting
-            app.MapHealthChecks("/health");
+	public static WebApplication MapDefaultEndpoints(this WebApplication app)
+	{
+		// Adding health checks endpoints to applications in non-development environments has security implications.
+		// See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
+		if (app.Environment.IsDevelopment())
+		{
+			// All health checks must pass for app to be considered ready to accept traffic after starting
+			app.MapHealthChecks("/health");
 
-            // Only health checks tagged with the "live" tag must pass for app to be considered alive
-            app.MapHealthChecks("/alive", new HealthCheckOptions
-            {
-                Predicate = r => r.Tags.Contains("live")
-            });
-        }
+			// Only health checks tagged with the "live" tag must pass for app to be considered alive
+			app.MapHealthChecks("/alive", new HealthCheckOptions
+			{
+				Predicate = r => r.Tags.Contains("live")
+			});
+		}
 
-        return app;
-    }
+		return app;
+	}
 }
