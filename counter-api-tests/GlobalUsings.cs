95e0643f5c36ae97ddac8e9ff464cbba93452861
thangchung <thangchung@ymail.com>
2024-08-20 21:25:53 +0700

    use WireMock.Net.Aspire #9
    Related Work Items: #8

---
+++ counter-api-tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using MassTransit.Testing;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.AspNetCore.TestHost;
+global using Microsoft.Extensions.DependencyInjection;
+global using Xunit;
+global using WireMock.Client.Builders;
